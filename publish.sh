fb0faa6a9e15eff4bebf342e2b36a3861b816061
florent champigny <florent.champigny@backelite.com>
2018-11-06 09:52:33 +0100

    updated build

--- publish.sh
+++ publish.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env bash
 ./gradlew clean
 ./gradlew build
+./gradlew compileDebugSources
+#./gradlew assembleAllIos_arm64
+#./gradlew assembleAllIos_x64
 
-#./gradlew javadocJarMain
+./gradlew javadocJarMain
 
+./gradlew generateMetadataFileForMavenJavaPublication
+
+./gradlew generateMetadataFileForMainPublication
+./gradlew generateMetadataFileForMainDebugIos_x64Publication
+./gradlew generateMetadataFileForMainDebugIos_arm64Publication
+
+./gradlew generatePomFileForMainDebugIos_x64Publication
+./gradlew generatePomFileForMainDebugIos_arm64Publication
 ./gradlew generatePomFileForMavenJavaPublication
 ./gradlew generatePomFileForMainPublication
-./gradlew :multiplatform-preferences-common:bintrayUpload
-
-#./gradlew sourcesJarMain
-#./gradlew sourcesJarMainIos_arm64
-#./gradlew sourcesJarMainIos_x64
-
-#./gradlew generatePomFileForMainPublication
-#./gradlew generateMetadataFileForMainPublication
-#./gradlew generatePomFileForMainDebugIos_x64Publication
-#./gradlew generateMetadataFileForMainDebugIos_x64Publication
-#./gradlew generatePomFileForMainDebugIos_arm64Publication
-#./gradlew generateMetadataFileForMainDebugIos_arm64Publication
-#./gradlew :ios:bintrayUpload
 
 ./gradlew generatePomFileForAndroidPublication
+
+./gradlew sourcesJarMain
+./gradlew sourcesJarMainIos_arm64
+./gradlew sourcesJarMainIos_x64
+
+./gradlew jar
+
+
+./gradlew sourcesJar
+
+./gradlew publishToMavenLocal
+
+
+./gradlew :ios:bintrayUpload
+./gradlew :multiplatform-preferences-common:bintrayUpload
 ./gradlew :android:bintrayUpload
