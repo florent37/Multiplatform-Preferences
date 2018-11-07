a0c577a3cc7666b78ca9d18b649b34ce9c7017b7
florent champigny <florent.champigny@backelite.com>
2018-11-07 10:32:21 +0100

    removed sample

--- publish.sh
+++ publish.sh
@@ -1,37 +1,44 @@
 #!/usr/bin/env bash
 ./gradlew clean
+./gradlew assemble
 ./gradlew build
-./gradlew compileDebugSources
+
+./gradlew :multiplatform-preferences-ios:publish
+./gradlew :multiplatform-preferences-ios:bintrayUpload
+./gradlew :multiplatform-preferences-common:bintrayUpload
+./gradlew :multiplatform-preferences-android:bintrayUpload
+
+#./gradlew compileDebugSources
 #./gradlew assembleAllIos_arm64
 #./gradlew assembleAllIos_x64
 
-./gradlew javadocJarMain
+#./gradlew javadocJarMain
 
-./gradlew generateMetadataFileForMavenJavaPublication
+#./gradlew generateMetadataFileForMavenJavaPublication
 
-./gradlew generateMetadataFileForMainPublication
-./gradlew generateMetadataFileForMainDebugIos_x64Publication
-./gradlew generateMetadataFileForMainDebugIos_arm64Publication
+#./gradlew generateMetadataFileForMainPublication
+#./gradlew generateMetadataFileForMainDebugIos_x64Publication
+#./gradlew generateMetadataFileForMainDebugIos_arm64Publication
 
-./gradlew generatePomFileForMainDebugIos_x64Publication
-./gradlew generatePomFileForMainDebugIos_arm64Publication
-./gradlew generatePomFileForMavenJavaPublication
-./gradlew generatePomFileForMainPublication
+#./gradlew generatePomFileForMainDebugIos_x64Publication
+#./gradlew generatePomFileForMainDebugIos_arm64Publication
+#./gradlew generatePomFileForMavenJavaPublication
+#./gradlew generatePomFileForMainPublication
 
-./gradlew generatePomFileForAndroidPublication
+#./gradlew generatePomFileForAndroidPublication
 
-./gradlew sourcesJarMain
-./gradlew sourcesJarMainIos_arm64
-./gradlew sourcesJarMainIos_x64
+#./gradlew sourcesJarMain
+#./gradlew sourcesJarMainIos_arm64
+#./gradlew sourcesJarMainIos_x64
 
-./gradlew jar
-
-
-./gradlew sourcesJar
-
-./gradlew publishToMavenLocal
+#./gradlew jar
 
 
-./gradlew :ios:bintrayUpload
-./gradlew :multiplatform-preferences-common:bintrayUpload
-./gradlew :android:bintrayUpload
+#./gradlew sourcesJar
+
+#./gradlew publishToMavenLocal
+
+
+#./gradlew :ios:bintrayUpload
+#./gradlew :multiplatform-preferences-common:bintrayUpload
+#./gradlew :android:bintrayUpload
