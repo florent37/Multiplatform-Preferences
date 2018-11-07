16170c785a68ce582194e9bc482d1808e5bbec46
florent champigny <florent.champigny@backelite.com>
2018-11-07 17:26:48 +0100

    refacto

--- publish.sh
+++ publish.sh
@@ -1,44 +1,5 @@
 #!/usr/bin/env bash
 ./gradlew clean
-./gradlew assemble
 ./gradlew build
-
-./gradlew :multiplatform-preferences-ios:publish
-./gradlew :multiplatform-preferences-ios:bintrayUpload
-./gradlew :multiplatform-preferences-common:bintrayUpload
-./gradlew :multiplatform-preferences-android:bintrayUpload
-
-#./gradlew compileDebugSources
-#./gradlew assembleAllIos_arm64
-#./gradlew assembleAllIos_x64
-
-#./gradlew javadocJarMain
-
-#./gradlew generateMetadataFileForMavenJavaPublication
-
-#./gradlew generateMetadataFileForMainPublication
-#./gradlew generateMetadataFileForMainDebugIos_x64Publication
-#./gradlew generateMetadataFileForMainDebugIos_arm64Publication
-
-#./gradlew generatePomFileForMainDebugIos_x64Publication
-#./gradlew generatePomFileForMainDebugIos_arm64Publication
-#./gradlew generatePomFileForMavenJavaPublication
-#./gradlew generatePomFileForMainPublication
-
-#./gradlew generatePomFileForAndroidPublication
-
-#./gradlew sourcesJarMain
-#./gradlew sourcesJarMainIos_arm64
-#./gradlew sourcesJarMainIos_x64
-
-#./gradlew jar
-
-
-#./gradlew sourcesJar
-
-#./gradlew publishToMavenLocal
-
-
-#./gradlew :ios:bintrayUpload
-#./gradlew :multiplatform-preferences-common:bintrayUpload
-#./gradlew :android:bintrayUpload
+./gradlew publishToMavenLocal
+./gradlew bintrayUpload
